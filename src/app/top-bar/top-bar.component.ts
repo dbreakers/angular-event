371304e950038517aed69af2f5bb9dc43b9e78a9
dbreakers <david.breakwell@gmail.com>
2022-03-09 10:51:47 +0000

    3 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+ 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-
+  @Output() toggleSidenav = new EventEmitter<void>();
 }
 
 
