5e1f6ed8a0e4e9870151db26bfa48eecb9ab3d8a
dbreakers <david.breakwell@gmail.com>
2022-03-27 10:22:03 +0100

    1 file modified, 2 added

--- src/app/members.service.ts
+++ src/app/members.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorHandler, HandleError } from 'http-error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
+  private handleError: HandleError;
   getMembersList(): Observable<Member[]> {
-    return this.http.get<Member[]>('/assets/people.json');
+    return this.http.get<Member[]>('/assets/people.json').pipe(
+      catchError(this.handleError('getHeroes', []))
+    );
   }
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,  httpErrorHandler: HttpErrorHandler) { 
+    this.handleError = httpErrorHandler.createHandleError('HeroesService');
+  }
 }
 
 export interface Member {
